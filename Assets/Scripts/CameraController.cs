2fabef5108026739eca616920d89cfd67d3fb95d
AlexKarpachov <146717790+AlexKarpachov@users.noreply.github.com>
2024-02-28 16:00:20 +0100

    Before towers
    map is ready. red + blue enemies are spawning. camera moves over the scene. Start to create towers

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+
+    private float initialHeight;
+
+    private void Start()
+    {
+        // To save initial camera high
+        initialHeight = transform.position.y;
+    }
+
+    private void LateUpdate()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        // To get direction vector to be able to move in the local coordinates
+        Vector3 localMoveDirection = transform.TransformDirection(new Vector3(horizontalInput, 0f, verticalInput)).normalized;
+
+        // to apply initial camera high
+        localMoveDirection.y = 0f;
+
+        // to move the camera in the local coordinates
+        transform.localPosition += moveSpeed * Time.deltaTime * localMoveDirection;
+
+        // to save initial camera high after movement
+        Vector3 newPosition = transform.position;
+        newPosition.y = initialHeight;
+        transform.position = newPosition;
+    }
+}
